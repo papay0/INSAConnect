4f8e3fc36c50543b8f7353994cfa2ee0a4182434
Arthur Papailhau <papailhau@gmail.com>
2016-01-20 18:09:46 +0100

    Initial Commit

---
+++ INSA_Portail_Captif/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  INSA_Portail_Captif
+//
+//  Created by Arthur Papailhau on 20/01/16.
+//  Copyright © 2016 Arthur Papailhau. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
